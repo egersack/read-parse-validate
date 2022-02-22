21d87b762f516eeb45b362ab7706b5f9a1b5bc26
egersack <egersack@mit.edu>
2022-02-22 10:16:48 -0500

    commit

--- 02_list_files/read_json_multiple.py
+++ 02_list_files/read_json_multiple.py
@@ -2,13 +2,31 @@
 # import package to read json
 # import package to walk file system
 # -------------------------------------
+import json
+import glob
 
 
 # -----------------------------------
 #  list all files
 # -----------------------------------
 
+pattern = './data/*/*.json'
+data = []
+
+for file in glob.glob(pattern):
+    data.append(file)
+
+print(data)
+
 
 # -----------------------------------
 #  loop through files, parse json
 # -----------------------------------
+
+def readJson(file):
+    with open(file) as p:
+        return json.load(p)
+
+for file in data:
+    banana = readJson(file)
+    print(banana)
